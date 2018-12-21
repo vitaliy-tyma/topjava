7872aa1fe0777a4afe620636c38ba8242da087d3
gkislin <github13>
2018-12-21 13:38:02 +0300

    Fix seqrch i18n and webapp-runner

--- hr.bat
+++ hr.bat
@@ -1,3 +1,2 @@
 call mvn -B -s settings.xml -DskipTests=true clean package
-rem call java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://user:password@localhost:5432/topjava" -jar target/dependency/webapp-runner.jar target/*.war
-call java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://user:password@localhost:5432/topjava" -classpath target/dependency webapp.runner.launch.Main target/*.war
+call java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://user:password@localhost:5432/topjava" -jar target/dependency/webapp-runner.jar target/*.war
